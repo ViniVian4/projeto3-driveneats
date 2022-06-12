512de9dd26f9ed2f5cdae82bbb032b396aa28309
Vinicius Viana <vianaxd99@gmail.com>
2022-06-12 04:42:43 -0300

    Todos os requisitos prontos

--- scripts.js
+++ scripts.js
@@ -1,34 +1,9 @@
 let itemsSelecionados = 0;
-
-function togglePratoSelecionado(itemEscolhido){ 
-    const pratoSelecionado = document.querySelector(".prato-selecionado");
-    if (pratoSelecionado!= null)
-        pratoSelecionado.classList.remove("prato-selecionado");
-    
-    const item = itemEscolhido;
-    item.classList.toggle("prato-selecionado");
-    atualizaItemsSelecionados();
-}
-
-function toggleBebidaSelecionada(itemEscolhido){ 
-    const bebidaSelecionada = document.querySelector(".bebida-selecionada");
-    if (bebidaSelecionada != null)
-        bebidaSelecionada.classList.remove("bebida-selecionada");
-    
-    const item = itemEscolhido;
-    item.classList.toggle("bebida-selecionada");
-    atualizaItemsSelecionados();
-}
-
-function toggleSobremesaSelecionada(itemEscolhido){ 
-    const sobremesaSelecionada = document.querySelector(".sobremesa-selecionada");
-    if (sobremesaSelecionada != null)
-        sobremesaSelecionada.classList.remove("sobremesa-selecionada");
-    
-    const item = itemEscolhido;
-    item.classList.toggle("sobremesa-selecionada");
-    atualizaItemsSelecionados();
-}
+let prato;
+let bebida;
+let sobremesa;
+let preco = 0;
+let msg;
 
 function atualizaItemsSelecionados(){
     itemsSelecionados++;
@@ -39,4 +14,69 @@ function atualizaItemsSelecionados(){
         botao.disabled = false;
         document.querySelector("button > p").innerHTML = "Fechar pedido";
     }
+}
+
+function transformaPreco (str){
+    let r = str;
+    r = r.slice(3, (str.length));
+    r = r.replace("," , ".");
+    r = Number(r);
+
+    return r;
+}
+
+function togglePratoSelecionado(itemEscolhido){ 
+    const pratoSelecionado = document.querySelector(".prato-selecionado");
+    if (pratoSelecionado!= null)
+        pratoSelecionado.classList.remove("prato-selecionado");
+    
+    const item = itemEscolhido;
+    item.classList.toggle("prato-selecionado");
+
+    prato = String(item.querySelector("p").innerHTML);
+    const precodoitem = transformaPreco(String(item.querySelector(".preco").innerHTML));
+    preco = preco + precodoitem;
+
+    atualizaItemsSelecionados();
+}
+
+function toggleBebidaSelecionada(itemEscolhido){ 
+    const bebidaSelecionada = document.querySelector(".bebida-selecionada");
+    if (bebidaSelecionada != null)
+        bebidaSelecionada.classList.remove("bebida-selecionada");
+    
+    const item = itemEscolhido;
+    item.classList.toggle("bebida-selecionada");
+
+    bebida = String(item.querySelector("p").innerHTML);
+    const precodoitem = transformaPreco(String(item.querySelector(".preco").innerHTML));
+    preco = preco + precodoitem;
+
+    atualizaItemsSelecionados();
+}
+
+function toggleSobremesaSelecionada(itemEscolhido){ 
+    const sobremesaSelecionada = document.querySelector(".sobremesa-selecionada");
+    if (sobremesaSelecionada != null)
+        sobremesaSelecionada.classList.remove("sobremesa-selecionada");
+    
+    const item = itemEscolhido;
+    item.classList.toggle("sobremesa-selecionada");
+
+    sobremesa = String(item.querySelector("p").innerHTML);
+    const precodoitem = transformaPreco(String(item.querySelector(".preco").innerHTML));
+    preco = preco + precodoitem;
+
+    atualizaItemsSelecionados();
+}
+
+function enviamsg() {
+    msg = "Olá, gostaria de fazer o pedido:\n - Prato: " 
+    + prato + "\n - Bebida: " + bebida + "\n - Sobremesa: " 
+    + sobremesa + "\n Total: R$ " + preco.toFixed(2);
+
+    const msgCodificada = encodeURIComponent(msg);
+    const linkwp = "https://wa.me/5531997975054?text=" + msgCodificada;
+    
+    return linkwp;
 }
